7f3caceabadd9fbfd5f5a4e0d98fa9c07d88bf40
JAYANTHNITW <kothapallijayanth521@gmail.com>
2023-08-29 17:16:36 +0530

    data transformation is added

--- src/EndtoEndTextSummerizer/entity/__init__.py
+++ src/EndtoEndTextSummerizer/entity/__init__.py
@@ -17,3 +17,10 @@ class DataValidationConfig:
     root_dir : Path
     STATUS_FILE: str
     ALL_REQUIRED_FILES: list
+
+#entity of data transformation
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir : Path
+    data_path: Path
+    tokenizer_name: Path
